19bdab28a5a42627706baf2bedc64ab3b1d054d5
Marcel Hernandez <e6990620@gmail.com>
2020-03-03 11:19:49 +0100

    * defined volume in the data dir * added locale

--- postgres/postgres.sh
+++ postgres/postgres.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-/usr/local/postgres/bin/initdb -k -D /var/lib/postgres/data
+/usr/local/pgsql/bin/initdb --locale=${LANG} --data-checksums -D /var/lib/postgres/data
 
-/usr/local/postgres/bin/postgres -D /var/lib/postgres/data
+/usr/local/pgsql/bin/postgres -D /var/lib/postgres/data
